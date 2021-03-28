558e2624c81bb69d4ee75c50eb201016b04a2808
Thomas Swisher <thomas@structuredseo.com>
2021-03-28 14:02:19 -0600

    Sunday Afternoon Update

--- app.js
+++ app.js
@@ -1,85 +1,113 @@
-let aliensLanded = 0
-let alShipsAcquired = 0
-let upgradeIn = 10
+let aliensHarvested = 0
+let x2Harvesters = 0
+let x5Harvesters = 0
+let totalHarvesters = 0
+let x2Adjustprice = 5
+let multiplier = 1
 let purchaseUpgrades = {
     alienShips1: {
+        price: 5,
+        quantity: 0,
+        multiplier: 2,
+        totalShips: 0
+    }, alienShips2: {
         price: 10,
         quantity: 0,
-        multiplier: 2
-    }, alienShips2: {
+        multiplier: 5,
+        totalShips: 0
+    }, alienShips3: {
         price: 15,
         quantity: 0,
-        multiplier: 10
-    }, alienShips3: {
-        price: 20,
-        quantity: 0,
-        multiplier: 15
+        multiplier: 10,
+        totalShips: 0
     }, alienShips4: {
         price: 20,
         quantity: 0,
-        multiplier: 2
+        multiplier: 30,
+        totalShips: 0
     }
 };
-
-
-// function multiplyUpgrade1() {
-//     if () { }
-// }
 
 // REVIEW
 function buyAlienShip1() {
     // NOTE access the object to get its price assign to variable
     let ship = purchaseUpgrades.alienShips1
 
-    if (aliensLanded >= ship.price) {
-        aliensLanded = aliensLanded - ship.price
-        ship.quantity++
-        ship.price *= 2
-        // Click Upgrade
-        alShipsAcquired += ship.multiplier
+    if (aliensHarvested >= ship.price) {
+        //  
+        aliensHarvested = aliensHarvested - ship.price
+        x2Harvesters = ship.quantity += 1
+        multiplier = ship.price *= 2
+        totalHarvesters += ship.multiplier
+        ship.price = x2Adjustprice
+        x2Adjustprice++
         update()
     }
 }
 
 function buyAlienShip2() {
-    if (aliensLanded >= 15) {
-        aliensLanded = aliensLanded - 15
-        alShipsAcquired = alShipsAcquired + 1
+    let ship = purchaseUpgrades.alienShips2
+
+    if (aliensHarvested >= ship.price) {
+        aliensHarvested = aliensHarvested - ship.price
+        x5Harvesters = ship.quantity + 1
+        multiplier = ship.price *= 5
+
+        // Click Upgrade
+        //totalHarvesters += ship.multiplier
         update()
     }
 }
 
 function buyAlienShip3() {
-    if (aliensLanded >= 20) {
-        aliensLanded = aliensLanded - 20
-        alShipsAcquired = alShipsAcquired + 1
-        update()
-    }
-}
-function buyAlienShip4() {
-    if (aliensLanded >= 30) {
-        aliensLanded = aliensLanded - 30
-        alShipsAcquired = alShipsAcquired + 1
+    let ship = purchaseUpgrades.alienShips3
+
+    if (aliensHarvested >= ship.price) {
+        aliensHarvested = aliensHarvested - ship.price
+        totalHarvesters = ship.quantity + 1
+        ship.price *= 2
+        //totalHarvesters += ship.multiplier
         update()
     }
 }
 
+function buyAlienShip4() {
+    let ship = purchaseUpgrades.alienShips4
+
+    if (aliensHarvested >= ship.price) {
+        aliensHarvested = aliensHarvested - ship.price
+        totalHarvesters = ship.quantity + 1
+        ship.price *= 2
+        totalHarvesters += ship.multiplier
+        update()
+    }
+}
+
+
 function mine() {
-    aliensLanded = aliensLanded + 1;
+    aliensHarvested = aliensHarvested + multiplier
     update()
 }
 
 function update() {
-    document.getElementById('aliensLanded').innerText = aliensLanded
-    document.getElementById('upgradeNumber').innerText = alShipsAcquired
-    // NOTE make sure to update the price and quantity for the ships as well as it might have gone up
+    document.getElementById('aliensHarvested').innerText = aliensHarvested
+    document.getElementById('harvester-x2').innerText = x2Harvesters
+    document.getElementById('harvester-x5').innerText = x5Harvesters
+    document.getElementById('harvester-x10').innerText = purchaseUpgrades.alienShips3.quantity
+    document.getElementById('harvester-x20').innerText = purchaseUpgrades.alienShips4.quantity
+    ///add others 
+    document.getElementById('upgradeNumber').innerText = x2Harvesters + x5Harvesters
+    document.getElementById('priceUpdate').innerText = x2Adjustprice
 
+}
+// NOTE make sure to update the price and quantity for the ships as well as it might have gone up
+
+function collectAutoUpgrades() {
+    buyAlienShip1()
 }
 
 function startInterval() {
-    collectionInterval = setInterval(collectAutoUpgrades,
-        alert(),
-        3000);
+    collectionInterval = setInterval(collectAutoUpgrades, 3000);
 }
 
 update()
